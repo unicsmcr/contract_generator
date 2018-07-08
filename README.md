51a63733a41216cf1adc2c93dd674ec122a7bc9c
Kristijonas <kzalys@gmail.com>
2018-07-08 13:31:52 +0100

    Add documentation

---
+++ README.md
@@ -0,0 +1,37 @@
+# PDF Contract Generator
+
+### Dependencies:
+* **Python 3** - download [here](https://www.python.org/download/releases/3.0/)
+* **pdfkit** - to install run ```pip install pdfkit```
+* **wkhtmltopdf** - installation instructions [here](https://wkhtmltopdf.org/)
+
+### Usage:
+The script takes an html template and a csv data file and produce a pdf file for each of the rows in the data file. The name of the generated file is: {firstColumnValueInDataFile}.pdf
+
+Script can be run with:
+```python pdfContract.py {template} {csvDataFile} {outputFolder}```
+> **NOTE:** the structure of the csv file is defined within the template
+
+### Creating templates:
+The templates for the script are defined in HTML and CSS
+
+Placeholders can be defined with the following syntax: ```[colx]``` where x is the column number (starting from 0) in the csv data file.
+
+> **EXAMPLE:**
+> The template:
+>```html
+><html>
+	><p>First name: [col0]</p>
+	><p>Last name: [col1]</p>
+></html>
+>```
+>and the data file:
+>```csv
+>Kristijonas,Zalys
+>```
+>would result in a pdf file:
+>**Kristijonas.pdf:**
+>```
+>First name:  Kristijonas
+>Last name: Zalys
+>```
--- pdfContract.py
+++ pdfContract.py
@@ -1,23 +1,30 @@
+# Usage: python pdfContract.py {template} {csvDataFile} {outputFolder}
+# Note: the structure of the csv data file is defined within the template
+# Dependencies:
+#    Python 3
+#    pdfkit (pip install pdfkit)
+#    wkhtmltopdf (wkhtmltopdf.org for installation instructions)
+
 import sys
 import pdfkit
 import csv
 import os
 
-
+# Makes contracts for each of the rows in dataFile with the template in templateFile and outputs everything to outputFolder
 def makeContracts(templateFile, dataFile, outputFolder):
-    contracts = csv.reader(open(dataFile, 'rt'))
-    for row in contracts:
+    contracts = csv.reader(open(dataFile, 'rt')) # Reading the dataFile
+    for row in contracts: # Going over all row in the datFile
         generateContract(templateFile, row, outputFolder)
 
-
-def generateContract(templateFile, collumns, outputFolder):
+# Creates a single pdf contract in the outputFolder with the given template and data
+def generateContract(templateFile, data, outputFolder):
     with open(templateFile, 'rt') as tFile:
-        template = tFile.read()
-        if not os.path.exists(outputFolder):
+        template = tFile.read() # Loading the template
+        if not os.path.exists(outputFolder): # Creating the outputFolder if it doesn't exist already
           os.mkdir(outputFolder)
-        for index, col in enumerate(collumns):
-            template = template.replace('[col' + str(index) + ']', col)
-        template = template.replace('Â£', u"\xA3")
-        pdfkit.from_string(template, outputFolder + '/' + collumns[0] + '.pdf')
+        for index, col in enumerate(data): # Going over all collumns in the data
+            template = template.replace('[col' + str(index) + ']', col) # Replacing the placeholders in the template
+        template = template.replace('Â£', u"\xA3") # For some reason can't encode the pound symbol properly, replacing it with its unicode character
+        pdfkit.from_string(template, outputFolder + '/' + data[0] + '.pdf') # Generating the pdf file
 
 makeContracts(sys.argv[1], sys.argv[2], sys.argv[3])
